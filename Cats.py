21f2e806a27201b7e75430200b78662c6c082fd4
antonkostenok <121963033+antonkostenok@users.noreply.github.com>
2023-03-16 09:56:43 +0500

    Add files via upload

---
+++ Cats.py
@@ -0,0 +1,9 @@
+from main import Cat
+
+cat_1 = Cat('Tom', 'male', 2)
+cat_2 = Cat('Лёля', 'девочка', 10)
+cat_3 = Cat("Мурзик", "М", 5)
+
+print(f"\nИмя: {cat_1.name}\nПол: {cat_1.gender}\nВозраст: {cat_1.age}")
+print(f"\nИмя: {cat_2.name}\nПол: {cat_2.gender}\nВозраст: {cat_2.age}")
+print(f"\nИмя: {cat_3.name}\nПол: {cat_3.gender}\nВозраст: {cat_3.age}")
